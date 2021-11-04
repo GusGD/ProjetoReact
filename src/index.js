3b895e934e684a399a28f4116c1904a6dacc30e9
Gustavo <47454571+GusGD@users.noreply.github.com>
2021-11-03 21:36:47 -0300

    Ajuste na index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.css';
 import Home from './templates/Home';
-
+ 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
